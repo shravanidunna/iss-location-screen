c03781b6fd5e49eb9381c05159fdbab104d726d8
shravanidunna <76036390+shravanidunna@users.noreply.github.com>
2023-07-22 14:49:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# iss-location-screen
+# PRO-C78-ISS-Tracker
+
+Class 78 final code
---
+++ screens/IssLocation.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+    ImageBackground,
+    StatusBar,
+    SafeAreaView,
+    Image,
+    Alert,
+    Platform
+} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import axios from "axios";
+
+export default class IssLocationScreen extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>ISS Location Screen!</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    titleContainer: {
+        flex: 0.1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white"
+    },
+    mapContainer: {
+        flex: 0.7
+    },
+    map: {
+        width: "100%",
+        height: "100%"
+    },
+    infoContainer: {
+        flex: 0.2,
+        backgroundColor: 'white',
+        marginTop: -10,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        padding: 30
+    },
+    infoText: {
+        fontSize: 15,
+        color: "black",
+        fontWeight: "bold"
+    }
+})
